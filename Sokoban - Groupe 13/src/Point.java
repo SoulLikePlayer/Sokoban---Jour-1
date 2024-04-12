da02921a98bb0203d74f054a8d1a1747c06718d0
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-12 08:48:25 +0200

    [PREMIER COMMIT] : Ajout des méthode de l'UML

---
+++ Sokoban - Groupe 13/src/Point.java
@@ -0,0 +1,24 @@
+public class Point {
+    private final int col;
+    private final int lig;
+
+    public Point(int col, int lig) {
+        this.col = col;
+        this.lig = lig;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Vérifie si l'objet est une instance de Point
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        // Cast de l'objet en Point
+        Point other = (Point) obj;
+
+        // Compare les attributs col et lig
+        return col == other.col && lig == other.lig;
+    }
+}
