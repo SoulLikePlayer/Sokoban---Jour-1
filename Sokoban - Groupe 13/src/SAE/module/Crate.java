95b0d2a2bb0fcda6464a9b5298f7e2e15ca6ed22
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-12 10:38:38 +0200

    Modification de la classe Crate et Player

--- Sokoban - Groupe 13/src/SAE/module/Crate.java
+++ Sokoban - Groupe 13/src/SAE/module/Crate.java
@@ -4,6 +4,12 @@ public class Crate extends Movable {
 	public Crate(int col, int lig) {
 		super(col,lig);
 	}
+
+	@Override
+	public void moveTo(int col, int lig) {
+		//TODO : à implémeter
+	}
+
 	public Crate(int col, int lig , boolean b) {
 		super(col,lig,b);
 	}
--- Sokoban - Groupe 13/src/SAE/module/Player.java
+++ Sokoban - Groupe 13/src/SAE/module/Player.java
@@ -6,6 +6,12 @@ public class Player extends Movable {
 	public Player(int col, int lig) {
 		super(col,lig);
 	}
+
+	@Override
+	public void moveTo(int col, int lig) {
+		//TODO : à implémeter
+	}
+
 	public Player(int col, int lig , boolean b) {
 		super(col,lig,b);
 	}
