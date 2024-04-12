fa2067691e0ec05ce8d1f1c683fb1513fd72401f
SorenGAUDRE <gaudre231@unicaen.fr>
2024-04-12 10:45:23 +0200

    [debut level]

---
+++ Sokoban - Groupe 13/src/SAE/module/Level.java
@@ -0,0 +1,17 @@
+package SAE.module;
+
+import java.util.List;
+
+public class Level {
+
+    private final GameRepresentation field ;
+    private final Point playerOrigin ;
+
+    private final List<Point> createOrigin ;
+
+    private final int nbLines ;
+
+    private final int nbColums ;
+
+
+}
