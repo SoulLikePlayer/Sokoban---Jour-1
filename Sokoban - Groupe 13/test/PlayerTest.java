5b458cedc18a9f6163e5d86a9369762019cb2e4f
SorenGAUDRE <gaudre231@unicaen.fr>
2024-04-12 13:11:08 +0200

    version official

--- Sokoban - Groupe 13/test/PlayerTest.java
+++ Sokoban - Groupe 13/test/PlayerTest.java
@@ -1,10 +1,10 @@
 import SAE.module.Player;
 import SAE.module.Point;
-import SAE.module.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class PlayerTest {
 
