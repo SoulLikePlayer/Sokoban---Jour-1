040aa6cf47ffc67cd819a664ccb0d7073f31a1c3
houlette <colin.houlette@etu.unicaen.fr>
2024-04-12 09:20:08 +0200

    Ajout de l'enum

---
+++ Sokoban - Groupe 13/src/GameRepresentation.java
@@ -0,0 +1,12 @@
+public enum GameRepresentation {
+
+    PLAYER("@"),
+    WALL('#'),
+    TARGET('.'),
+    BOX('$'),
+    PLAYER_ON_TARGET('+'),
+    BOX_ON_TARGET('*'),
+    EMPTY(' '),
+    EMPTY_EXTERIOR('X');
+
+}
